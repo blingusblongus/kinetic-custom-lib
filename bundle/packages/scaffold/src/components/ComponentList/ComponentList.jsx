f38ea586e20cb9ca96dcb8e13f80d6690fab7ff7
Nick Allen <nallen@esolutionsone.com>
2022-03-22 13:04:30 -0500

    scaffold actually configured

---
+++ bundle/packages/scaffold/src/components/ComponentList/ComponentList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { fetchForm } from '@kineticdata/react';
+
+const ComponentList = () => {
+  console.log(fetchForm);
+  return <h1>ComponentList</h1>;
+};
+
+export default ComponentList;
