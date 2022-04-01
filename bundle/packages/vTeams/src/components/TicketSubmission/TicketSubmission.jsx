f340a02f631739bd0a13beaba2e5848d8c9fe7f8
Nick Allen <nallen@esolutionsone.com>
2022-04-01 11:50:26 -0500

    added short and full description fields

--- bundle/packages/vTeams/src/components/TicketSubmission/TicketSubmission.jsx
+++ bundle/packages/vTeams/src/components/TicketSubmission/TicketSubmission.jsx
@@ -8,16 +8,40 @@ import {
   InputLabel,
 } from '@mui/material';
 
+import './TicketSubmission.css';
+
 const TicketSubmission = ({ ticket }) => {
   return (
     <Box>
-      <Paper elevation={8}>
+      <div className="form-wrapper">
         <div>TicketSubmission</div>
         <FormGroup row>
           <InputLabel id="estimate-label">Estimate</InputLabel>
           <Select variant="standard" labelId="estimate-label" />
         </FormGroup>
-      </Paper>
+
+        <div className="flex flex-column form-section">
+          <label className="label-description" for="short-description">
+            Short Description
+          </label>
+          <textarea
+            name="short-description"
+            rows={4}
+            placeholder="Add a more detailed description..."
+          />
+        </div>
+
+        <div className="flex flex-column form-section">
+          <label className="label-description" for="full-description">
+            Full Description
+          </label>
+          <textarea
+            name="full-description"
+            rows={8}
+            placeholder="Add a more detailed description..."
+          />
+        </div>
+      </div>
     </Box>
   );
 };
