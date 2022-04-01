3a59bf67c4e3682cf58a6e5828530e79865ed098
Nick Allen <nallen@esolutionsone.com>
2022-04-01 11:59:54 -0500

    add page bg color and fixed subheader drop shadow

--- bundle/packages/vTeams/src/components/TicketSubmission/TicketSubmission.jsx
+++ bundle/packages/vTeams/src/components/TicketSubmission/TicketSubmission.jsx
@@ -12,37 +12,35 @@ import './TicketSubmission.css';
 
 const TicketSubmission = ({ ticket }) => {
   return (
-    <Box>
-      <div className="form-wrapper">
-        <div>TicketSubmission</div>
-        <FormGroup row>
-          <InputLabel id="estimate-label">Estimate</InputLabel>
-          <Select variant="standard" labelId="estimate-label" />
-        </FormGroup>
+    <div className="form-wrapper">
+      <div>TicketSubmission</div>
+      <FormGroup row>
+        <InputLabel id="estimate-label">Estimate</InputLabel>
+        <Select variant="standard" labelId="estimate-label" />
+      </FormGroup>
 
-        <div className="flex flex-column form-section">
-          <label className="label-description" for="short-description">
-            Short Description
-          </label>
-          <textarea
-            name="short-description"
-            rows={4}
-            placeholder="Add a more detailed description..."
-          />
-        </div>
-
-        <div className="flex flex-column form-section">
-          <label className="label-description" for="full-description">
-            Full Description
-          </label>
-          <textarea
-            name="full-description"
-            rows={8}
-            placeholder="Add a more detailed description..."
-          />
-        </div>
+      <div className="flex flex-column form-section">
+        <label className="label-description" for="short-description">
+          Short Description
+        </label>
+        <textarea
+          name="short-description"
+          rows={4}
+          placeholder="Add a more detailed description..."
+        />
       </div>
-    </Box>
+
+      <div className="flex flex-column form-section">
+        <label className="label-description" for="full-description">
+          Full Description
+        </label>
+        <textarea
+          name="full-description"
+          rows={8}
+          placeholder="Add a more detailed description..."
+        />
+      </div>
+    </div>
   );
 };
 
