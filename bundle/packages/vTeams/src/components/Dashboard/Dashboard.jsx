ba65ca0572f6fd502d63b9372163e9e6793a65f2
Nick Allen <nallen@esolutionsone.com>
2022-04-04 10:18:19 -0500

    add datagrid

--- bundle/packages/vTeams/src/components/Dashboard/Dashboard.jsx
+++ bundle/packages/vTeams/src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 import './Dashboard.scss';
 
 const Dashboard = () => {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 30 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 140,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 50,
+      editable: true,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: params =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
   return (
     <div className="page-panel">
       <div className="grid" id="dashboard-grid">
         <div className="card-wrapper">
-          <div>Left Content</div>
+          <div style={{ height: 500, padding: 20 }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              density="compact"
+              autoHeight
+            />
+          </div>
         </div>
         <div className="flex flex-column">
-          <div className="card-wrapper">
-            <div>Right Upper Content</div>
-          </div>
+          <div className="card-wrapper">Right Upper Content</div>
           <div className="card-wrapper">
             <div>Right Lower Content</div>
           </div>
