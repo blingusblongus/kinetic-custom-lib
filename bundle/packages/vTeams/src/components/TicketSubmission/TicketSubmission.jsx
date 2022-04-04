801040b474691178e75f2571894a3329fbde348c
Nick Allen <nallen@esolutionsone.com>
2022-04-04 15:37:03 -0500

    add basic knowledge widget tab

--- bundle/packages/vTeams/src/components/TicketSubmission/TicketSubmission.jsx
+++ bundle/packages/vTeams/src/components/TicketSubmission/TicketSubmission.jsx
@@ -5,6 +5,7 @@ import './TicketSubmission.css';
 import Priority from '../Priority/Priority';
 import TeamsButton from '../TeamsButton/TeamsButton';
 import { TextField } from '@mui/material';
+import { bgColorPrimary } from '../../App.scss';
 
 const TicketSubmission = ({ ticket }) => {
   const [shortDescription, setShortDescription] = useState('');
@@ -19,69 +20,79 @@ const TicketSubmission = ({ ticket }) => {
   });
 
   return (
-    <div className="page-panel">
-      <div className="card-wrapper">
-        <div className="label-description">Ticket #{ticket?.number}</div>
-        <div className="submission-grid">
-          <InputLabel id="estimate-label">Estimate</InputLabel>
+    <>
+      <div className="knowledge-widget knowledge-widget-tab">
+        <div className="knowledge-widget knowledge-widget-content" />
+      </div>
 
-          {/* <input
+      <div className="page-panel">
+        <div className="card-wrapper">
+          <div className="label-description">Ticket #{ticket?.number}</div>
+          <div className="submission-grid">
+            <InputLabel id="estimate-label">Estimate</InputLabel>
+
+            {/* <input
             type="number"
             value={estimate}
             onChange={e => setEstimate(e.target.value)}
           /> */}
-          <TextField
-            name="Estimated Duration"
-            variant="standard"
-            type="number"
-            placeholder="Estimated Hours"
-            sx={{
-              "input[type='number']:not(.form-control)": {
-                padding: 0,
-                border: 'none',
-                height: '2.1rem',
-              },
-            }}
-          />
-          <div />
+            <TextField
+              name="Estimated Duration"
+              variant="standard"
+              type="number"
+              placeholder="Estimated Hours"
+              sx={{
+                "input[type='number']:not(.form-control)": {
+                  padding: 0,
+                  border: 'none',
+                  height: '2.1rem',
+                },
+              }}
+            />
+            <div />
 
-          <InputLabel id="priority-label">Priority</InputLabel>
+            <InputLabel id="priority-label">Priority</InputLabel>
 
-          <Select variant="standard" labelId="priority-label" defaultValue={1}>
-            {prioritySelections}
-          </Select>
+            <Select
+              variant="standard"
+              labelId="priority-label"
+              defaultValue={1}
+            >
+              {prioritySelections}
+            </Select>
 
-          <InputLabel id="date-label">Due Date</InputLabel>
-          <input type="date" />
-        </div>
+            <InputLabel id="date-label">Due Date</InputLabel>
+            <input type="date" />
+          </div>
 
-        <div className="flex flex-column form-section">
-          <label className="label-description" htmlFor="short-description">
-            Short Description
-          </label>
-          <textarea
-            name="short-description"
-            rows={4}
-            placeholder="Add a more detailed description..."
-          />
-        </div>
+          <div className="flex flex-column form-section">
+            <label className="label-description" htmlFor="short-description">
+              Short Description
+            </label>
+            <textarea
+              name="short-description"
+              rows={4}
+              placeholder="Add a more detailed description..."
+            />
+          </div>
 
-        <div className="flex flex-column form-section">
-          <label className="label-description" htmlFor="full-description">
-            Full Description
-          </label>
-          <textarea
-            name="full-description"
-            rows={8}
-            placeholder="Add a more detailed description..."
-          />
-        </div>
+          <div className="flex flex-column form-section">
+            <label className="label-description" htmlFor="full-description">
+              Full Description
+            </label>
+            <textarea
+              name="full-description"
+              rows={8}
+              placeholder="Add a more detailed description..."
+            />
+          </div>
 
-        <div className="flex flex-right">
-          <TeamsButton>Submit</TeamsButton>
+          <div className="flex flex-right">
+            <TeamsButton>Submit</TeamsButton>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
