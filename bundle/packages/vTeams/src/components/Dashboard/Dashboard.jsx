3f07a03e009f904828885da65283ed03a535676d
Nick Allen <nallen@esolutionsone.com>
2022-04-11 13:27:50 -0500

    refactor Dashboard

--- bundle/packages/vTeams/src/components/Dashboard/Dashboard.jsx
+++ bundle/packages/vTeams/src/components/Dashboard/Dashboard.jsx
@@ -23,6 +23,10 @@ import { SearchNormal1 } from 'iconsax-react';
 import { fontMedium } from '../../App.scss';
 import RecentlyViewed from '../RecentlyViewed/RecentlyViewed';
 
+import { columns, rows, data, daysArr } from './modules.js';
+
+import { addBackground } from './plugins.js';
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -33,86 +37,7 @@ ChartJS.register(
   Legend,
 );
 
-const addBackground = {
-  id: 'custom_canvas_background_color',
-  beforeDraw: chart => {
-    const { left, top, width, height } = chart.chartArea;
-    console.log('chart data', chart);
-    const ctx = chart.canvas.getContext('2d');
-    ctx.save();
-    ctx.globalCompositeOperation = 'destination-over';
-
-    var gradient = ctx.createLinearGradient(0, top, 0, height);
-
-    // Add three color stops
-    gradient.addColorStop(0.35, 'rgba(255,255,255,.2');
-    gradient.addColorStop(1, 'rgba(190,201,224,.39');
-    ctx.fillStyle = gradient;
-
-    ctx.fillRect(left, top, width, height);
-    ctx.restore();
-  },
-};
-
 const Dashboard = () => {
-  // Example Data
-  // Table
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 30 },
-    {
-      field: 'firstName',
-      headerName: 'First name',
-      width: 140,
-      editable: true,
-    },
-    {
-      field: 'lastName',
-      headerName: 'Last name',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 50,
-      editable: true,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: params =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-
-  // Burndown graph
-  const data = [
-    { x: 1, y: 50 },
-    { x: 10, y: 45 },
-    { x: 13, y: 24 },
-    { x: 16, y: 20 },
-    { x: 24, y: 12 },
-  ];
-  // ^^^^^^^^^^^End Example Data
-
-  const daysArr = [...Array(31).keys()];
-
   return (
     <div className="dashboard page-panel">
       <div className="grid" id="dashboard-grid">
