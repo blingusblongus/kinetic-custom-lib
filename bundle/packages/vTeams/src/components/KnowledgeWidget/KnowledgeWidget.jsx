12028f63438d1a94b74c09f0dcd898929d873620
Nick Allen <nallen@esolutionsone.com>
2022-04-13 14:31:44 -0500

    knowledge widget revamped

--- bundle/packages/vTeams/src/components/KnowledgeWidget/KnowledgeWidget.jsx
+++ bundle/packages/vTeams/src/components/KnowledgeWidget/KnowledgeWidget.jsx
@@ -11,29 +11,63 @@ const KnowledgeWidget = () => {
 
   const iconConfig = {
     color: 'white',
-    position: 'absolute',
-    top: '4px',
-    left: '4px',
+    top: 8,
+    left: 5,
     cursor: 'pointer',
     opacity: open ? 0 : 1,
     transition: 'all .8s',
+    position: 'absolute',
+    fontSize: 20,
   };
 
+  const articles = [
+    {
+      title: 'First Article',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+    {
+      title: 'Second Article',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+    {
+      title: 'Third Article',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+  ];
+
   return (
-    <div className={`knowledge-widget knowledge-widget--tab ${widgetOpen}`}>
-      <div className="knowledge-widget knowledge-widget--content" />
-      <LightbulbOutlinedIcon
-        sx={{ ...iconConfig, opacity: open ? 0 : 1 }}
-        onClick={toggleOpen}
-      />
-      <ChevronRight
-        sx={{
-          ...iconConfig,
-          opacity: open ? 1 : 0,
-          transform: !open && 'rotate(270deg)',
-        }}
-        onClick={toggleOpen}
-      />
+    <div className={`knowledge-widget ${widgetOpen}`}>
+      <div className="widget-tab">
+        <div className="widget-icon">
+          <LightbulbOutlinedIcon
+            sx={{ ...iconConfig, opacity: open ? 0 : 1 }}
+            onClick={toggleOpen}
+          />
+          <ChevronRight
+            sx={{
+              ...iconConfig,
+              opacity: open ? 1 : 0,
+              transform: !open && 'rotate(270deg)',
+            }}
+            onClick={toggleOpen}
+          />
+        </div>
+        <div className="widget-title">Knowledge Center</div>
+      </div>
+
+      <div className="knowledge-widget--content">
+        {articles.map((article, i) => {
+          return (
+            <div className="knowledge-article" key={i}>
+              <div className="article-title">{article.title}</div>
+              <div className="article-content">{article.content}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
