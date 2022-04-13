e21ccad6a039b0ce1aefca354a7620f71b53d408
Nick Allen <nallen@esolutionsone.com>
2022-04-13 13:15:21 -0500

    knowledge widget tinkering

--- bundle/packages/vTeams/src/components/KnowledgeWidget/KnowledgeWidget.jsx
+++ bundle/packages/vTeams/src/components/KnowledgeWidget/KnowledgeWidget.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import ChevronRight from '@mui/icons-material/ChevronRight';
 import './_KnowledgeWidget.scss';
 
 const KnowledgeWidget = () => {
+  const [open, setOpen] = useState(false);
+  const widgetOpen = open ? 'knowledge-widget--open' : '';
+
+  const toggleOpen = () => setOpen(!open);
+
+  const iconConfig = {
+    color: 'white',
+    position: 'absolute',
+    top: '4px',
+    left: '4px',
+    cursor: 'pointer',
+    opacity: open ? 0 : 1,
+    transition: 'all .8s',
+  };
+
   return (
-    <div className="knowledge-widget knowledge-widget--tab">
-      <div className="knowledge-widget knowledge-widget--content">
-        Some Extra info goes here
-      </div>
+    <div className={`knowledge-widget knowledge-widget--tab ${widgetOpen}`}>
+      <div className="knowledge-widget knowledge-widget--content" />
       <LightbulbOutlinedIcon
+        sx={{ ...iconConfig, opacity: open ? 0 : 1 }}
+        onClick={toggleOpen}
+      />
+      <ChevronRight
         sx={{
-          color: 'white',
-          position: 'absolute',
-          top: '4px',
-          left: '4px',
+          ...iconConfig,
+          opacity: open ? 1 : 0,
+          transform: !open && 'rotate(270deg)',
         }}
+        onClick={toggleOpen}
       />
     </div>
   );
