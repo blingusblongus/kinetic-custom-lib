59981ac9087173b50b9878e06e2b1499f0d5b609
Nick Allen <nallen@esolutionsone.com>
2022-04-21 12:59:38 -0500

    add parsePriority util

--- bundle/packages/customUtils/utils.js
+++ bundle/packages/customUtils/utils.js
@@ -1,3 +1,4 @@
+import Priority from "../vTeams/src/components/Priority/Priority";
 
 /**
  * Parses list of submissions retrieved with the Kinetic searchSubmission API,
@@ -7,23 +8,37 @@
  * @returns {array} array with format [columnHeaders, rows]
  */
 export const parseSubsToTablegrid = (submissions) => {
-    const [cs, rs] = [[],[]];
+    const [cs, rs] = [[], []];
+    console.log(submissions[0])
 
     if (submissions.length > 1) {
-      for (let key in submissions[0].values) {
-        cs.push({ field: key, headerName: key, width: 100 })
-      }
-      for (let sub of submissions) {
-        let vals = { ...sub.values, id: sub.id }
-  
-        // for (let key in vals) {
-        //   console.log(vals[key]);
-        //   // if (Array.isArray(vals[key])) {
-            
-        //   // }
-        // }
-        rs.push(vals);
-      }
+        for (let key in submissions[0].values) {
+            cs.push({ field: key, headerName: key, width: 100 })
+        }
+        for (let sub of submissions) {
+            let vals = {
+                ...sub.values,
+                id: sub.id,
+            }
+
+            // for (let key in vals) {
+            //   console.log(vals[key]);
+            //   // if (Array.isArray(vals[key])) {
+
+            //   // }
+            // }
+            rs.push(vals);
+        }
     }
     return [cs, rs];
-  }
+}
+
+/**
+ * Takes in a number 1-3 and returns a string priority
+ * @param {number} num Priority level, 1 as lowest
+ * @returns String value representing priority
+ */
+export const parsePriority = (num) => {
+    const priorities = ['Low', 'Medium', 'High'];
+    return priorities[num];
+}
