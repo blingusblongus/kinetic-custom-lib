fcf998b9dbf2b5abd4d10b11d3a94e888c4883a1
Nick Allen <nallen@esolutionsone.com>
2022-04-21 12:46:38 -0500

    add parseSubsToTablegrid docstring

--- bundle/packages/customUtils/utils.js
+++ bundle/packages/customUtils/utils.js
@@ -1,9 +1,16 @@
+
+/**
+ * Parses list of submissions retrieved with the Kinetic searchSubmission API,
+ * and formats them for use in a MUI tablegrid component
+ * 
+ * @param {array} submissions Array of kinetic submissions with included values 
+ * @returns {array} array with format [columnHeaders, rows]
+ */
 export const parseSubsToTablegrid = (submissions) => {
     const [cs, rs] = [[],[]];
 
     if (submissions.length > 1) {
       for (let key in submissions[0].values) {
-        console.log(key);
         cs.push({ field: key, headerName: key, width: 100 })
       }
       for (let sub of submissions) {
