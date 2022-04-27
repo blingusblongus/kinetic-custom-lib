35b4cc83777573e0828eb9aa08992fc8168c374f
Nick Allen <nallen@esolutionsone.com>
2022-04-27 11:12:57 -0500

    add fade-in to new comments

--- bundle/packages/vTeams/src/components/Activities/Activities.jsx
+++ bundle/packages/vTeams/src/components/Activities/Activities.jsx
@@ -1,10 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { SubmissionSearch, searchSubmissions } from '@kineticdata/react';
+import {
+  SubmissionSearch,
+  searchSubmissions,
+  createSubmission,
+} from '@kineticdata/react';
 import Activity from './Activity/Activity.jsx';
 import './_Activities.scss';
+import TeamsButton from '../TeamsButton/TeamsButton.jsx';
+import { useSelector } from 'react-redux';
 
 const Activities = ({ id }) => {
   const [activities, setActivities] = useState([]);
+  const [commentText, setCommentText] = useState('');
+  const [reFetch, setReFetch] = useState(false);
+
+  const userProfile = useSelector(store => store.app.profile);
+  console.log(userProfile);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('submitted');
+
+    const values = {
+      'Ticket ID': id,
+      Comment: commentText,
+      Commenter: userProfile.displayName,
+    };
+
+    createSubmission({
+      kappSlug: 'vteams',
+      formSlug: 'activity',
+      values,
+    }).then(submission => {
+      console.log('submission sent', submission);
+      setCommentText('');
+      setReFetch(!reFetch);
+    });
+  };
+
   useEffect(
     () => {
       const fetchActivities = async () => {
@@ -27,15 +60,27 @@ const Activities = ({ id }) => {
       };
       fetchActivities();
     },
-    [id],
+    [id, reFetch],
   );
 
   console.log(activities);
   return (
     <div className="card-wrapper activity-feed">
-      <h2>Activity Feed</h2>
-      {activities.map(submission => {
-        console.log(submission);
+      <h2>Notes and Comments</h2>
+      <div>
+        <div>Add new Comment:</div>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            rows={3}
+            value={commentText}
+            onChange={e => setCommentText(e.target.value)}
+          />
+          <TeamsButton mode="dark" sx={{ margin: '10px' }} type="submit" />
+        </form>
+      </div>
+
+      {// Activity log
+      activities.map(submission => {
         return <Activity key={submission.handle} submission={submission} />;
       })}
     </div>
