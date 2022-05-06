c57ab64c0fe6a297ad946480ffb553aca973df75
Nick Allen <nallen@esolutionsone.com>
2022-05-06 11:05:43 -0500

    add footer link placeholders and version number

--- bundle/packages/vTeams/src/components/Footer/Footer.jsx
+++ bundle/packages/vTeams/src/components/Footer/Footer.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import './Footer.scss';
 
 export const Footer = () => {
-  return <footer />;
+  return (
+    <footer className="flex flex-align-vert flex-center">
+      <div>
+        <div>esolutionsONE - vTEAMS</div>
+        <div className="flex flex-center footer-link-container">
+          -
+          <a>Contact Us</a>
+          -
+          <a>Privacy Policy</a>
+          -
+          <a>Report an Issue</a>
+          -
+          <a>Version 0.1.2</a>
+          -
+        </div>
+      </div>
+    </footer>
+  );
 };
