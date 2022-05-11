554e8636d6b9e9427292a37e934fb334d8ae545f
Nick Allen <nallen@esolutionsone.com>
2022-05-11 10:42:08 -0500

    possible redirect bugfix (re-enable backend preview form)

---
+++ bundle/packages/vTeams/src/components/BurndownFulfiller/BurndownFulfiller.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { SubmissionSearch, searchSubmissions } from '@kineticdata/react';
+import { getPaginated } from '../../lib/utils';
+import { VTEAMS } from '../../../globals/globals';
+
+const BurndownFulfiller = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchBurndownInfo = async () => {
+      let search = new SubmissionSearch().include('values').build();
+
+      const clientInfo = await getPaginated({
+        kapp: VTEAMS.KAPPSLUG,
+        form: VTEAMS.CLIENT_FORM_SLUG,
+        search,
+      });
+      console.log(clientInfo);
+
+      // Init hashmap to track burndown data
+      const hash = {};
+      for (let submission of clientInfo) {
+        console.log('submission', submission);
+        const org = submission.values['Organization'];
+        if (!hash[org]) {
+          hash[org] = {
+            submissions: [],
+            ['Hours Worked']: 0,
+            ['Monthly Hours']: Number(submission.values['Monthly Hours']),
+          };
+        }
+      }
+
+      console.log('hash', hash);
+
+      search = new SubmissionSearch()
+        .eq('values[isWorkLog]', 'true')
+        .include('values')
+        .build();
+
+      const workLogs = await getPaginated({
+        kapp: VTEAMS.KAPPSLUG,
+        form: VTEAMS.ACTIVITY_FORM_SLUG,
+        search,
+      });
+
+      console.log('workLogs', workLogs);
+      for (let log of workLogs) {
+        const org = log.values['Organization'];
+        hash[org].submissions.push(log);
+        hash[org]['Hours Worked'] += Number(log.values['Hours Worked']);
+      }
+
+      console.log(hash);
+      setData(hash);
+    };
+
+    fetchBurndownInfo();
+  }, []);
+
+  return (
+    <div>
+      {Object.keys(data).map((org, i) => {
+        console.log(data[org]);
+        const total = data[org]['Monthly Hours'];
+        const utilized = data[org]['Hours Worked'];
+        const remaining = total - utilized;
+        return (
+          <div key={i}>
+            {org}: {remaining} hours remaining of {total} hours.
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BurndownFulfiller;
