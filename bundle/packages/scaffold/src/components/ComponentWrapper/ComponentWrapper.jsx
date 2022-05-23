d6c12dde341338df915eba106a299aa61305dd8f
Nick Allen <nallen@esolutionsone.com>
2022-05-23 09:30:42 -0500

    remove scaffold and customQueue kapps

--- bundle/packages/scaffold/src/components/ComponentWrapper/ComponentWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Paper, Box } from '@mui/material';
-
-const ComponentWrapper = ({ component, sx, elevation }) => {
-  return <Box sx={{ mt: '1rem' }}>{component}</Box>;
-};
-
-export default ComponentWrapper;
--- bundle/packages/scaffold/src/redux/hooks/hooks.js
+++
@@ -1,8 +0,0 @@
-import {
-  createDispatchHook,
-  createSelectorHook,
-} from '../../../node_modules/react-redux';
-import { context } from '../store';
-
-export const useSelector = createSelectorHook(context);
-export const useDispatch = createDispatchHook(context);
